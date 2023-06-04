bbcc80fc1a1f59e45e910ad8c0a446e874231955
adam <apawlowski@fream.pl>
2023-06-04 20:58:41 +0200

    alpha menu

--- board.py
+++ board.py
@@ -16,9 +16,11 @@ pygame.init()
 # Set the width and height of the screen
 board_size = 640
 tile_size = board_size // 8
-sidebar_width = 300
-screen = pygame.display.set_mode((board_size + sidebar_width, board_size))
-pygame.display.set_caption("Chessboard")
+sidebar_width = 200
+window_width = board_size + sidebar_width
+window_height = board_size
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Chess Game")
 
 # Set the clock font and size
 clock_font = pygame.font.Font(None, 36)
@@ -70,6 +72,12 @@ def handle_promotion(move):
     clicked_promotion_piece = None
 
 
+# Create a clock object to control the frame rate
+clock = pygame.time.Clock()
+
+# Store the game state
+game_state = "MainMenu"
+
 # Main game loop
 running = True
 while running:
@@ -77,129 +85,139 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # Handle mouse events
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Get the tile position where the mouse was clicked
-            x, y = pygame.mouse.get_pos()
-            if x > board_size:
-                # Check if the click is inside the promotion menu
-                menu_x = board_size + 10
-                menu_y = (board_size - tile_size) // 2
-                menu_width = sidebar_width - 20
-                menu_height = tile_size
-                if menu_x <= x <= menu_x + menu_width and menu_y <= y <= menu_y + menu_height:
-                    clicked_index = (x - menu_x) // tile_size
-                    clicked_promotion_piece = promotion_menu_pieces[clicked_index]
+            if game_state == "MainMenu":
+                # Check if the "Start Game" button was clicked
+                if 300 <= event.pos[0] <= 500 and 250 <= event.pos[1] <= 300:
+                    game_state = "Chessboard"
+
+            elif game_state == "Chessboard":
+                # Handle chessboard events
+                # Get the tile position where the mouse was clicked
+                x, y = pygame.mouse.get_pos()
+                if x > board_size:
+                    # Check if the click is inside the promotion menu
+                    menu_x = board_size + 10
+                    menu_y = (board_size - (tile_size * len(promotion_menu_pieces))) // 2
+                    menu_width = sidebar_width - 20
+                    menu_height = tile_size * len(promotion_menu_pieces)
+                    if menu_x <= x <= menu_x + menu_width and menu_y <= y <= menu_y + menu_height:
+                        clicked_index = (y - menu_y) // tile_size
+                        clicked_promotion_piece = promotion_menu_pieces[clicked_index]
+                        continue
+                    # Ignore clicks on the sidebar
                     continue
-                # Ignore clicks on the sidebar
-                continue
-            row = 7 - y // tile_size
-            col = x // tile_size
+                row = 7 - y // tile_size
+                col = x // tile_size
 
-            # Get the piece on the clicked tile
-            square = chess.square(col, row)
-            piece = board.piece_at(square)
+                # Get the piece on the clicked tile
+                square = chess.square(col, row)
+                piece = board.piece_at(square)
 
-            if selected_piece is None:
-                # If no piece is selected, check if the clicked tile contains a piece
-                if piece is not None:
-                    # Store the selected piece and its position
-                    selected_piece = piece
-                    selected_piece_pos = square
-            else:
-                # If a piece is already selected, try to make a move
-                move = chess.Move(selected_piece_pos, square)
-
-                if selected_piece.piece_type == chess.PAWN and (
-                        chess.square_rank(move.to_square) == 0 or chess.square_rank(move.to_square) == 7
-                ):
-                    # Handle pawn promotion
-                    handle_promotion(move)
-                elif selected_piece.piece_type == chess.PAWN and board.is_capture(move) and (
-                        chess.square_rank(move.to_square) == 0 or chess.square_rank(move.to_square) == 7
-                ):
-                    # Handle capture to promotion
-                    handle_promotion(move)
+                if selected_piece is None:
+                    # If no piece is selected, check if the clicked tile contains a piece
+                    if piece is not None:
+                        # Store the selected piece and its position
+                        selected_piece = piece
+                        selected_piece_pos = square
                 else:
-                    if move in board.legal_moves:
-                        # If the move is valid, update the board
-                        board.push(move)
-                        notation.append(move)
+                    # If a piece is already selected, try to make a move
+                    move = chess.Move(selected_piece_pos, square)
 
-                # Reset the selected piece and its position
-                selected_piece = None
-                selected_piece_pos = None
+                    if selected_piece.piece_type == chess.PAWN and (
+                            chess.square_rank(move.to_square) == 0 or chess.square_rank(move.to_square) == 7
+                    ):
+                        # Handle pawn promotion
+                        handle_promotion(move)
+                    elif selected_piece.piece_type == chess.PAWN and board.is_capture(move) and (
+                            chess.square_rank(move.to_square) == 0 or chess.square_rank(move.to_square) == 7
+                    ):
+                        # Handle capture to promotion
+                        handle_promotion(move)
+                    else:
+                        if move in board.legal_moves:
+                            # If the move is valid, update the board
+                            board.push(move)
+                            notation.append(move)
 
-    # Clear the screen with a gray background
+                    # Reset the selected piece and its position
+                    selected_piece = None
+                    selected_piece_pos = None
+
+    # Clear the screen
     screen.fill(GRAY)
 
-    # Draw the chessboard tiles
-    for row in range(8):
-        for col in range(8):
-            x = col * tile_size
-            y = (7 - row) * tile_size
-            if (row + col) % 2 == 0:
-                pygame.draw.rect(screen, LIGHT_BROWN, (x, y, tile_size, tile_size))
-            else:
-                pygame.draw.rect(screen, DARK_BROWN, (x, y, tile_size, tile_size))
+    if game_state == "MainMenu":
+        # Draw the main menu
+        start_game_button = pygame.draw.rect(screen, BLUE, (300, 250, 200, 50))
+        start_game_text = clock_font.render("Start Game", True, WHITE)
+        screen.blit(start_game_text, (325, 260))
 
-    # Draw the chess pieces on the board
-    for square in chess.SQUARES:
-        rank = chess.square_rank(square)
-        file = chess.square_file(square)
-        x = file * tile_size
-        y = (7 - rank) * tile_size
+    elif game_state == "Chessboard":
+        # Draw the chessboard
+        for row in range(8):
+            for col in range(8):
+                x = col * tile_size
+                y = (7 - row) * tile_size
+                if (row + col) % 2 == 0:
+                    pygame.draw.rect(screen, LIGHT_BROWN, (x, y, tile_size, tile_size))
+                else:
+                    pygame.draw.rect(screen, DARK_BROWN, (x, y, tile_size, tile_size))
 
-        # Draw the chess piece if it exists on the current square
-        piece = board.piece_at(square)
-        if piece is not None:
-            piece_image = piece_images[(piece.piece_type, piece.color)]
-            screen.blit(piece_image, (x, y))
+        # Draw the chess pieces on the board
+        for square in chess.SQUARES:
+            rank = chess.square_rank(square)
+            file = chess.square_file(square)
+            x = file * tile_size
+            y = (7 - rank) * tile_size
 
-    # Draw a blue border around the selected piece
-    if selected_piece is not None:
-        selected_x = chess.square_file(selected_piece_pos) * tile_size
-        selected_y = (7 - chess.square_rank(selected_piece_pos)) * tile_size
-        pygame.draw.rect(screen, BLUE, (selected_x, selected_y, tile_size, tile_size), 5)
+            # Draw the chess piece if it exists on the current square
+            piece = board.piece_at(square)
+            if piece is not None:
+                piece_image = piece_images[(piece.piece_type, piece.color)]
+                screen.blit(piece_image, (x, y))
 
-    # Draw the sidebar
-    pygame.draw.rect(screen, WHITE, (board_size, 0, sidebar_width, board_size))
+        # Draw a blue border around the selected piece
+        if selected_piece is not None:
+            selected_x = chess.square_file(selected_piece_pos) * tile_size
+            selected_y = (7 - chess.square_rank(selected_piece_pos)) * tile_size
+            pygame.draw.rect(screen, BLUE, (selected_x, selected_y, tile_size, tile_size), 5)
 
-    # Draw the promotion menu on the sidebar
-    menu_x = board_size + 10
-    menu_y = (board_size - tile_size) // 2
-    menu_width = sidebar_width - 20
-    menu_height = tile_size
-    pygame.draw.rect(screen, BLUE if clicked_promotion_piece is not None else WHITE,
-                     (menu_x, menu_y, menu_width, menu_height))
-    menu_piece_width = menu_width // len(promotion_menu_pieces)
-    for i, piece_type in enumerate(promotion_menu_pieces):
-        piece_image = piece_images[(piece_type, chess.WHITE)]
-        piece_rect = piece_image.get_rect()
-        piece_rect.center = (menu_x + (i + 0.5) * menu_piece_width, menu_y + tile_size / 2)
-        screen.blit(piece_image, piece_rect)
+        # Draw the sidebar
+        pygame.draw.rect(screen, WHITE, (board_size, 0, sidebar_width, board_size))
 
-    # Draw the clock
-    elapsed_time = int(time.time() - start_time)
-    minutes = elapsed_time // 60
-    seconds = elapsed_time % 60
-    clock_text = f"Time: {minutes:02d}:{seconds:02d}"
-    clock_surface = clock_font.render(clock_text, True, BLACK)
-    screen.blit(clock_surface, (board_size + 10, 10))
+        # Draw the promotion menu on the sidebar
+        menu_x = board_size + 10
+        menu_y = (board_size - (tile_size * len(promotion_menu_pieces))) // 2
+        pygame.draw.rect(screen, LIGHT_BROWN,
+                         (menu_x, menu_y, sidebar_width - 20, tile_size * len(promotion_menu_pieces)))
+        for i, piece_type in enumerate(promotion_menu_pieces):
+            piece_image = piece_images[(piece_type, chess.WHITE)]
+            piece_x = menu_x + 10
+            piece_y = menu_y + i * tile_size
+            screen.blit(piece_image, (piece_x, piece_y))
 
-    # Draw the notation of played moves as a list
-    notation_x = board_size + 10
-    notation_y = menu_y + tile_size + 20
-    for i in range(0, len(notation), 2):
-        move1 = notation[i]
-        move2 = notation[i + 1] if i + 1 < len(notation) else ""
-        move_text = f"{i // 2 + 1}. {move1} {move2}"
-        notation_surface = notation_font.render(move_text, True, BLACK)
-        screen.blit(notation_surface, (notation_x, notation_y))
-        notation_y += notation_surface.get_height() + 5
+        # Draw the notation
+        notation_x = board_size + 10
+        notation_y = board_size - 10
+        for i, move in enumerate(notation):
+            notation_text = notation_font.render(str(move), True, BLACK)
+            screen.blit(notation_text, (notation_x, notation_y - (i + 1) * 30))
+
+        # Draw the current move count
+        move_count_text = clock_font.render("Move: " + str(len(notation)), True, BLACK)
+        screen.blit(move_count_text, (notation_x, notation_y - (len(notation) + 1) * 30))
+
+        # Draw the clock
+        elapsed_time = time.time() - start_time
+        clock_text = clock_font.render("Time: " + time.strftime("%H:%M:%S", time.gmtime(elapsed_time)), True, BLACK)
+        screen.blit(clock_text, (notation_x, 10))
 
     # Update the display
     pygame.display.flip()
+
+    # Control the frame rate
+    clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
