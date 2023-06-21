661d1c71d6eccfe485138307b2effde966ea412d
adam <apawlowski@fream.pl>
2023-06-21 20:46:22 +0200

    refactor train into oop_train

---
+++ oop_train.py
@@ -0,0 +1,92 @@
+import random
+import chess
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+class ChessAI:
+    def __init__(self):
+        self.model = None
+
+    def train(self, num_samples=10, max_depth=100):
+        X_train = []
+        y_train = []
+
+        for _ in range(num_samples):
+            position = self.random_board(max_depth)
+            encoded_position = self.encode_position(position)
+            evaluation = random.random()  # Placeholder evaluation
+            X_train.append(encoded_position)
+            y_train.append(evaluation)
+
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+
+        model = Sequential()
+        model.add(Dense(32, activation='relu', input_shape=(64,)))
+        model.add(Dense(2064, activation='softmax'))  # Output a probability distribution over moves
+
+        model.compile(optimizer='adam', loss='mse')
+        model.fit(X_train, y_train, epochs=10)
+
+        self.model = model
+
+    @staticmethod
+    def random_board(max_depth=100):
+        board = chess.Board()
+        depth = random.randrange(40, max_depth)
+
+        for _ in range(depth):
+            all_moves = list(board.legal_moves)
+            random_move = random.choice(all_moves)
+            board.push(random_move)
+            if board.is_game_over():
+                break
+
+        return board
+
+    @staticmethod
+    def encode_position(position):
+        board_representation = np.zeros(64, dtype=np.int8)
+        piece_map = position.piece_map()
+
+        for square, piece in piece_map.items():
+            index = square // 8 * 8 + square % 8
+            piece_type = piece.piece_type
+            board_representation[index] = piece_type
+
+        return board_representation
+
+    @staticmethod
+    def decode_move(prediction, legal_moves):
+        move_probs = prediction.flatten()
+        legal_move_indices = [i for i, move in enumerate(legal_moves)]
+        move_index = np.argmax(move_probs[np.array(legal_move_indices)])  # Select the move with the highest probability
+        selected_move = legal_moves[legal_move_indices[move_index]]
+
+        return selected_move
+
+    def predict_move(self, board):
+        encoded_position = self.encode_position(board)
+        input_data = np.array([encoded_position])
+        prediction = self.model.predict(input_data)
+        legal_moves = list(board.legal_moves)
+        selected_move = self.decode_move(prediction, legal_moves)
+
+        return selected_move
+
+
+if __name__ == '__main__':
+    chess_ai = ChessAI()
+    chess_ai.train(num_samples=10, max_depth=100)
+
+    # Create a sample chess position
+    board = chess_ai.random_board()
+
+    # Print the board
+    print(board)
+
+    # Predict and print the move
+    predicted_move = chess_ai.predict_move(board)
+    print("Predicted Move:", predicted_move)
